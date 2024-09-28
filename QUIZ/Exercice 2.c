644b5a56789e1aa997c8f7373da665cb0e9cf1ad
Hafsa_19 <hafsa.elbahali@gmail.com>
2024-09-28 10:59:29 +0200

    QUIZ/Exercice 2.c

--- QUIZ/Exercice 2.c
+++ QUIZ/Exercice 2.c
@@ -7,7 +7,7 @@ int main()
     int age ;
         printf("veuillez entrer l'age : ");
         scanf("%d",&age);
-     if (age <= 18);
+     if (age <= 18)
         printf("entrer la categorie est vous n etes pas eligible pour voter");
      else
         printf("entrer la categorie est vous etes eligible pour voter");
