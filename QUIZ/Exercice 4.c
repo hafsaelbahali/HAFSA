59537ffcc98508703deee3497c701bf0040843ff
Hafsa_19 <hafsa.elbahali@gmail.com>
2024-10-02 10:40:31 +0200

    quiz

--- QUIZ/Exercice 4.c
+++ QUIZ/Exercice 4.c
@@ -1,22 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int somme=0, moyenne, n;
-    printf("entrer la taille:\n");
-    scanf("%d",&n);
-    int tab[5];
-    printf("entrer les elements du tableau:\n");
 
-    for(int i=0; i<n; i++) {
-    printf("l'element %d", i+1);
-    scanf("%d",&tab[i]);
+int main() {
 
-        somme+=tab[i];
+    int nombres[5];
+    int i;
 
+
+    for(i=0; i<5; i++){
+        printf("Entrez le nombre %d: ",i+1);
+        scanf("%d", &nombres[i]);
     }
-    moyenne= somme/5;
-    printf("la moyenne est %d\n", moyenne);
+
+    int plus_grand = nombres[0];
+    int plus_petit = nombres[0];
+
+    for (i=1; i<5; i++) {
+        if (nombres[i] > plus_grand){
+            plus_grand = nombres[i];
+        }
+        if
+            (nombres[i] < plus_petit);{
+            plus_petit = nombres[i];
+        }
+    }
+
+    printf("Le plus grand nombre est: %d\n", plus_grand);
+    printf("Le plus petit nombre est: %d\n", plus_petit);
     return 0;
-}
+    }
+
+
