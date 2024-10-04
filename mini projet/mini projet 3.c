f5eaa4b456fc2c4bc3b10987600e744600385864
Hafsa_19 <hafsa.elbahali@gmail.com>
2024-10-04 12:34:18 +0200

    mini projet

--- mini projet/mini projet 3.c
+++ mini projet/mini projet 3.c
@@ -6,7 +6,7 @@ int main() {
     char prenom[50];
     char telephone[15];
     int age;
-    char date[20]; // Format DD-MM-YYYY
+    char date[20];
     int patient = 0;
     int choix;
     int choix_statut;
@@ -17,11 +17,11 @@ int main() {
 
 
     do{
-   printf("\n***Gestion des Réservations de Rendez-vous Dentaires ***\n");
+   printf("\n***Gestion des Reservations de Rendez-vous Dentaires ***\n");
 
-    printf("1. Ajouter une réservation :\n");
-    printf("2.Modifier une réservation :\n");
-    printf("3. Supprimer une réservation :\n");
+    printf("1. Ajouter une reservation :\n");
+    printf("2.Modifier une reservation :\n");
+    printf("3. Supprimer une reservation :\n");
     printf("4. Afficher les détails d'une réservation :\n");
     printf("5. Tri des réservation :\n");
     printf("6. Recherche des réservation :\n");
@@ -47,23 +47,38 @@ int main() {
 
         switch(choix_statut){
             case 1:
-                printf("Valide");
+                printf("Valide \n");
             break;
-            case 2: printf("Reporte");
+            case 2: printf("Reporte \n");
             break;
-            case 3:printf("Annule");
+            case 3:printf("Annule \n");
             break;
-            case 4:printf("Traite");
+            case 4:printf("Traite \n");
             break;
             default:
             printf("le numero invalide\n");
             break;
         }
+            case 2:
+            break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                printf(" Au revoir!\n");
+                break;
 
-   }
+
+
 }
-
-while(choix!=0);
+}while(choix!=0);
     return 0;
-}
+    }
 
